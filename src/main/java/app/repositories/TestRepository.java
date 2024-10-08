5c7376c365a3059ee0e7e4cbeb80d1f422505c47
zg04ckpt <nguyenhc424@gmail.com>
2024-10-08 21:39:20 +0700

    merge index.html

--- src/main/java/app/repositories/TestRepository.java
+++ src/main/java/app/repositories/TestRepository.java
@@ -5,15 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 //định nghĩa các truy vấn tới CSDL để lấy hoặc thay đổi dữ liệu
 public interface TestRepository extends JpaRepository<Test, String > {
-    /* nếu cần truy vấn phức tạp thì thêm, ví dụ truy vấn sử dụng cú pháp tên tham số:
-        @Query("SELECT u FROM User u WHERE u.prop1 = :prop1 AND u.prop2 = :prop2")
-        List<User> findByProp1AndProp2(@Param("prop1") String prop1, @Param("prop2") boolean prop2);
-        (Luu ý User sẽ là tên class trong Model chứ không phải ten bảng trong DB)
+    /*
+        viết các phương thức truy cập database ngoài các pt sau:
+            + save(Entity e): Lưu / tạo mới đối tượng e
+            + saveAll(List<Entity> es): Lưu / tạo nhiều
+            + getById(String id): Tim và lấy về đối tượng bằng Id
+            + getAll(): Lấy tất cả
+            + deleteById(String id): Xóa đôí tượng bằng id
+            + count(): trả về số dòng ~ số lượng entity
+            + ... (tóm lại cái nào có sẵn trong repository. thì ko cần định nghĩa nữa)
+        cách định nghĩa phương thức mới: <search GPT với key: các cách tự định nghĩa phương thức trong JPA>
     */
 
-    /* 1 kiểu truy vấn mà ko cần @Query là viết tên phương thức theo định dạng
-        VD: tìm kiếm theo giá trị của thuộc tính bất kì của Test:
-            Test findBy + <tên_thuộc_tính_viết_hoa_chữ_cái_đầu>(Kiểu_của_thuộc_tính giá_trị_cần_tìm);
-    */
     Test findByProp1(String prop1);
 }
