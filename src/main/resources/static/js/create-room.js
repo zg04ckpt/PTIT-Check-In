93db3b58ae6fbc8f20d4e952b168f57182087a49
zg04ckpt <nguyenhc424@gmail.com>
2024-10-10 16:13:50 +0700

    merge all

---
+++ src/main/resources/static/js/create-room.js
@@ -0,0 +1,166 @@
+// ------------------------------- GET LIST ----------------------------
+console.log("script works!");
+
+const getListBtn = document.getElementById('getListBtn')
+const getListDialog = document.getElementById('getListDialog')
+const getListCloseBtn = document.getElementById('getListCloseBtn')
+const idColumnInput = document.getElementById('idColumnInput')
+const nameColumnInput = document.getElementById('nameColumnInput')
+const startRowInput = document.getElementById('startRowInput')
+const getListFromExcelBtn = document.getElementById('getListFromExcelBtn')
+const previewTable = document.getElementById('previewTable')
+const count = document.getElementById('count')
+const saveBtn = document.getElementById('saveBtn')
+const successLabel = document.getElementById('successLabel')
+const list = document.getElementById('list')
+
+getListFromExcelBtn.onclick = e => {
+    if(!idColumnInput.value || !nameColumnInput.value || !startRowInput.value) {
+        alert("Vui lòng điền vị trí cột, dòng")
+        e.preventDefault();
+    }
+}
+
+let previewData = [];
+getListFromExcelBtn.onchange = e => {
+    const file = e.target.files[0];
+    if(file) {
+        const reader = new FileReader();
+        reader.onload = e => {
+            const data = new Uint8Array(e.target.result);
+            const workbook = XLSX.read(data, { type: 'array' });
+
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+
+            const rowData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+
+            const idCol = idColumnInput.value - 1;
+            const nameCol = nameColumnInput.value - 1;
+            const startRow = startRowInput.value - 1;
+            for(let i = startRow; i < rowData.length; i++) {
+                previewTable.querySelector('tbody').replaceChildren();
+                const row = previewTable.insertRow();
+                row.insertCell(0).textContent = i;
+                row.insertCell(1).textContent = rowData[i][idCol];
+                row.insertCell(2).textContent = rowData[i][nameCol];
+                previewData.push({
+                    id: rowData[i][idCol], 
+                    name: rowData[i][nameCol]
+                });
+                
+            }
+            
+            count.innerText = "Số lượng: " + (rowData.length - startRow);
+        }
+        reader.readAsArrayBuffer(file);
+    }
+}
+
+saveBtn.onclick = e => {
+    e.preventDefault();
+
+    if(previewData.length == 0) {
+        showNotification("Danh sách trống");
+        return;
+    }
+    
+    console.log(list);
+
+    list.replaceChildren();
+    previewData.forEach((e, index) => {
+        const id = document.createElement('input');
+        id.type = 'text'
+        id.name = 'attendees[' + index + '].id';
+        id.value = e.id;
+        
+        const name = document.createElement('input');
+        name.type = 'text'
+        name.name = 'attendees[' + index + '].name';
+        name.value = e.name;
+
+        list.appendChild(id);
+        list.appendChild(name);
+    });
+
+    successLabel.innerText = "Lấy danh sách thành công";
+    successLabel.parentElement.classList.toggle('d-none');
+}
+
+// ----------------------------- GET LOCATION -----------------------
+const getLocationDialog = document.getElementById('getLocationDialog')
+const getLocationBtn = document.getElementById('getLocationBtn')
+const getLocationCloseBtn = document.getElementById('getLocationCloseBtn')
+const getLocationMethodSelect = document.getElementById('getLocationMethodSelect')
+const locationSelect = document.getElementById('locationSelect')
+const positionInput = document.getElementById('positionInput')
+const QR = document.getElementById('QR')
+const saveLocationBtn = document.getElementById('saveLocationBtn')
+const rangeSelect = document.getElementById('rangeSelect')
+const rangeInput = document.getElementById('rangeInput')
+
+
+getLocationMethodSelect.onchange = e => {
+    const select = e.target.value; 
+    if(select == 1) {
+        locationSelect.hidden = false;
+        positionInput.hidden = true;
+        QR.hidden = true;
+    } else if(select == 2) {
+        locationSelect.hidden = true;
+        positionInput.hidden = false;
+        QR.hidden = true;
+    } else {
+        locationSelect.hidden = true;
+        positionInput.hidden = true;
+        QR.hidden = false;
+    }
+}
+
+// -------------------------- OTHER -----------------------------
+const startTimeInput = document.getElementById('check2')
+const endTimeInput = document.getElementById('check3')
+
+startTimeInput.onchange = e => {
+    room.startTime = startTimeInput.value;
+}
+
+endTimeInput.onchange = e => {
+    room.endTime = endTimeInput.value;
+}
+
+function showNotification(msg) {
+    var modal = new bootstrap.Modal(document.getElementById('notification'));
+    document.getElementById('noti-msg').innerText = msg;
+    modal.show();
+}
+
+
+// --------------------------- Hưng --------------------------------
+const checkbox1 = document.getElementById('check1');
+const additionalInfo1 = document.getElementById('info1');
+checkbox1.addEventListener('change', () => {
+    additionalInfo1.classList.toggle('hidden');
+});
+
+const checkbox2 = document.getElementById('check2');
+const additionalInfo2 = document.getElementById('info2');
+checkbox2.addEventListener('change', () => {
+    additionalInfo2.classList.toggle('hidden');
+    additionalInfo2.parentElement.querySelector('p').classList.toggle('hidden')
+});
+
+const checkbox3 = document.getElementById('check3');
+const additionalInfo3 = document.getElementById('info3');
+checkbox3.addEventListener('change', () => {
+    additionalInfo3.classList.toggle('hidden');
+    additionalInfo3.parentElement.querySelector('p').classList.toggle('hidden')
+});
+
+document.getElementById('getListBtn').onclick = e => {
+    e.preventDefault();
+}
+
+document.getElementById('check7').onclick = e => {
+    e.preventDefault();
+}
